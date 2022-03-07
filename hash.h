b6aad6993f08e0fe6bf57298d0ed782e6413e52a
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-07 15:35:07 -0600

    Create hash.h

---
+++ hash.h
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+class hashmap{
+public:
+  private:
+    static const int tableSize = 100;
+    struct Student {
+      char firstName[20];
+      char lastName[20];
+      int studentId;
+      float GPA;
+      Student* next;
+    };
+ 
+  
+  public:
+    hashmap(int ntableSize);
+    int Hash(char* key);
+    void addStudent(char* firstName, char* lastName, int studentId, float GPA);
+    int numItemsIndex(int index);
+    void print();
+    Student* HashTable[tableSize];
+
+};
+
+//end
