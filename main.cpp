0913f68b3b49d7ec846c04f161023eb594d38659
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-13 14:33:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,18 @@ while(true){
     cout << "Type 1 for add, 2 for print, 3 for delete, or 4 for quit" << endl;
     cin >> input;
     cin.get();
+	*Hashy.addStudent("Paul", "Locha", 1, 1.11);
+    Hashy.addStudent("Kim", "Iced Mocha", 2, 2.22);
+    Hashy.addStudent("Emma", "Strawberry Smoothie", 3, 3.33);
+    Hashy.addStudent("Annie", "Hot Chocolate", 4, 4.44);
+    Hashy.addStudent("Sarah", "Passion Tea", 5, 5.55);
+    Hashy.addStudent("Pepper", "Caramel Mocha", 6, 6.66);
+    Hashy.addStudent("Mike", "Chai Tea", 7, 7.77);
+    Hashy.addStudent("Steve", "Apple Cider", 8, 8.88);
+    Hashy.addStudent("Bill", "Root Beer", 9, 9.99);
+    Hashy.addStudent("Marie", "Skinny Latte", 11, 1.123);
+    Hashy.addStudent("Susan", "Water", 12, 2.123);
+    Hashy.addStudent("Joe", "Green Tea", 13, 3.123);
      //asks user for details of student 
     if(input == 1){
 		cout << "Please enter the student's first name" << endl;
