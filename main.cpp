2208d8217192a1028c3c9b53ea243bc5cab5674e
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-13 14:59:25 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,18 +36,6 @@ while(true){
  		cin >> G;
 	  	cin.get();
 		Hashy.addStudent(fN, lN, sI, G);
-	    Hashy.addStudent("Paul", "Locha", 1, 1.11);
-    Hashy.addStudent("Kim", "Iced Mocha", 2, 2.22);
-    Hashy.addStudent("Emma", "Strawberry Smoothie", 3, 3.33);
-    Hashy.addStudent("Annie", "Hot Chocolate", 4, 4.44);
-    Hashy.addStudent("Sarah", "Passion Tea", 5, 5.55);
-    Hashy.addStudent("Pepper", "Caramel Mocha", 6, 6.66);
-    Hashy.addStudent("Mike", "Chai Tea", 7, 7.77);
-    Hashy.addStudent("Steve", "Apple Cider", 8, 8.88);
-    Hashy.addStudent("Bill", "Root Beer", 9, 9.99);
-    Hashy.addStudent("Marie", "Skinny Latte", 11, 1.123);
-    Hashy.addStudent("Susan", "Water", 12, 2.123);
-    Hashy.addStudent("Joe", "Green Tea", 13, 3.123);
     }
     //prints linked list	
     if(input == 2){
@@ -65,6 +53,14 @@ while(true){
     //exits function
     if(input == 4){
       break;
+    }
+    //random student gen
+    if(input == 5){
+      cout << "how many students would you like to add?" << endl;
+      int num;
+      cin >> num;
+      cin.get();
+	   
     }
   }
   return 0;  	
