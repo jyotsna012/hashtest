0a43b957bc478ce58a6d7c24a177eb91683ccefa
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-13 17:34:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,7 +43,10 @@ int main(){
     hashmap Hashy(tablesize);
 while(true){
     int input;
-    cout << "Type 1 for add, 2 for print, 3 for delete, or 4 for quit" << endl;
+    int input2;
+    cout << "Would you like to add any random computer generated students? Type 1 for yes and 2 for no" << endl;
+    cin >> input2;
+    cout << "now its time to manually add students. Type 1 for add, 2 for print, 3 for delete, or 4 for quit" << endl;
     cin >> input;
     cin.get();
     //asks user for details of student 
@@ -82,17 +85,18 @@ while(true){
     if(input == 4){
       break;
     }
+    if(input2 == 2){
+   
+    }
     //random student gen
-    if(input == 5){
-      cout << "how many students would you like to add?" << endl;
+    if(input2 == 1){
+      cout << "enter the number of computer generated students you would like to enter. " << endl;
       int num;
       cin >> num;
       cin.get(); 
       char done[20] = "done";
     
       for(int z =0; z < num; z++){
-      
-      
       
       string s1(first[z]);
       char p1[s1.length()];
@@ -124,36 +128,3 @@ float RandomFloat(float a, float b) {
     float r = random * diff;
     return a + r;
 }
-
-/*void rehash(int tablesize, hashmap Hashy){
-int oldtablesize = tablesize;
-tablesize = tablesize+tablesize;
-hashmap HashyTwo(tablesize);
-for(int i =0; i < oldtablesize; i++){
-  char newfirstName[80];
-  char newlastName[80];
-  int newid;
-  float newgpa;
-  if(Hashy.HashTable[i]->studentId != 0){
-    strcpy(newfirstName, Hashy.HashTable[i]-> firstName);  
-    strcpy(newlastName, Hashy.HashTable[i]->lastName);  
-    newid = Hashy.HashTable[i]->studentId;
-    newgpa = Hashy.HashTable[i]->GPA;
-    HashyTwo.addStudent(newfirstName, newlastName, newid, newgpa); 
-  }
-}
-   HashyTwo.print();
-}*/
-
-    /*Hashy.addStudent("Paul", "Locha", 1, 1.11);
-    Hashy.addStudent("Kim", "Iced Mocha", 2, 2.22);
-    Hashy.addStudent("Emma", "Strawberry Smoothie", 3, 3.33);
-    Hashy.addStudent("Annie", "Hot Chocolate", 4, 4.44);
-    Hashy.addStudent("Sarah", "Passion Tea", 5, 5.55);
-    Hashy.addStudent("Pepper", "Caramel Mocha", 6, 6.66);
-    Hashy.addStudent("Mike", "Chai Tea", 7, 7.77);
-    Hashy.addStudent("Steve", "Apple Cider", 8, 8.88);
-    Hashy.addStudent("Bill", "Root Beer", 9, 9.99);
-    Hashy.addStudent("Marie", "Skinny Latte", 11, 1.123);
-    Hashy.addStudent("Susan", "Water", 12, 2.123);
-    Hashy.addStudent("Joe", "Green Tea", 13, 3.123);*/
